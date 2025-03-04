2d666506a013fb97db0966d08f4346320be8d80c
DUOLabs333 <dvdugo333@gmail.com>
2025-03-04 13:14:29 -0500

    Finish implementing the CRUD operations for the user accounts

--- backend/users.py
+++ backend/users.py
@@ -1,6 +1,6 @@
 from utils import *
 
-@endpoint("/signup", ["email", "password"], ["token"])
+@endpoint("/signup", ["email", "password"])
 def signup():
     user=supabase.auth.sign_up({"email": email, "password": password})
     if user["error"]!="":
@@ -9,13 +9,33 @@ def signup():
         response=table("users").insert({"id": user["id"]}).execute()
         if (response["error"]!=""):
             error=response["error"]
-        else:
-            token=user["access_token"]
 
 @endpoint("/login", ["email", "password"], ["token"])
 def login():
     user=supabase.auth.sign_up({"email": email, "password": password})
     error=user["error"]
+
+    token=user["access_token"]
+#signup, signup, login, modify, signup with new details, signup with old details, login with new details, delete both
+@endpoint("/modify", ["email", "password"], ["email"])
+def modify():
+    #email and/or password should be the NEW email and password, respectively
+
+    update_dict={}
+    if email is not None:
+        update_dict["email"]=email
+    if password is not None:
+        update_dict["password"]=password
+    response=auth.update_user(update_dict)
+
+    error=response["error"] #TODO: Add code to endpoint to not send error if empty
+
+    if email is not None:
+        email=response["email"]
+
+@endpoint("/delete", [])
+def delete():
+   auth.admin.delete_user(auth.get_user(token)["user"]["id"])
 
 
 
