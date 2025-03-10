569ec3515cf4a8a6b863b514125fc62e3e7596a6
DUOLabs333 <dvdugo333@gmail.com>
2025-03-10 10:42:18 -0400

    Add some more tests

--- backend/users.py
+++ backend/users.py
@@ -1,5 +1,6 @@
 from utils import *
 
+#We'll let users sign up without verifying email; however, to receive emails (including to reset password), they must verify it first
 @endpoint("/signup", ["email", "password"])
 def signup():
     user=client.auth.sign_up({"email": email, "password": password})
@@ -13,6 +14,8 @@ def login():
 
     token=user["access_token"]
 #signup, signup, login, login (doesn't exist) modify, signup with new details, signup with old details, login with new details, delete both
+
+#It's unclear what should happen if the email you pass is the same as your current email (either an error, or a no-op is fine)
 @endpoint("/modify", ["email", "password"], ["email"])
 def modify():
     #email and/or password should be the NEW email and password, respectively
@@ -29,7 +32,12 @@ def modify():
 
 @endpoint("/delete", [])
 def delete():
-   client.auth.admin.delete_user(auth.get_user(token)["user"]["id"])
+    user_id=auth.get_user(token)["user"]["id"]
+    client.auth.admin.delete_user(user_id)
+
+    client.table("users").delete().eq("id", user_id).execute()
+
+
 
 
 
