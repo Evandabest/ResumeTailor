fe3297f468333c4abda9b674cc57911983e2a8eb
DUOLabs333 <dvdugo333@gmail.com>
2025-03-10 19:23:11 -0400

    Fix a bug in the tests where the client was making GET requests, as opposed to POST requests

--- backend/users.py
+++ backend/users.py
@@ -3,17 +3,16 @@ from utils import *
 #We'll let users sign up without verifying email; however, to receive emails (including to reset password), they must verify it first
 @endpoint("/signup", ["email", "password"])
 def signup():
-    user=client.auth.sign_up({"email": email, "password": password}).user
+    user=User.auth.sign_in_with_password({"email": email, "password": password}).user
 
-    client.table("users").insert({"id": user.id}).execute()
+    User.table("users").insert({"id": user.id}).execute()
 
 
 @endpoint("/login", ["email", "password"], ["token"])
 def login():
-    user=client.auth.sign_up({"email": email, "password": password})
+    user=User.auth.sign_up({"email": email, "password": password})
 
     token=user.session.access_token
-#signup, signup, login, login (doesn't exist) modify, signup with new details, signup with old details, login with new details, delete both
 
 #It's unclear what should happen if the email you pass is the same as your current email (either an error, or a no-op is fine)
 @endpoint("/modify", ["email", "password"], ["email"])
@@ -25,17 +24,18 @@ def modify():
         update_dict["email"]=email
     if password is not None:
         update_dict["password"]=password
-    response=client.auth.update_user(update_dict)
+    response=User.auth.update_user(update_dict)
 
     if email is not None:
         email=response.email
 
 @endpoint("/delete", [])
 def delete():
-    user_id=auth.get_user(token).user.id
-    client.auth.admin.delete_user(user_id)
+    print(token)
+    user_id=User.auth.get_user(token).user.id
+    Admin.auth.admin.delete_user(user_id)
 
-    client.table("users").delete().eq("id", user_id).execute()
+    Admin.table("users").delete().eq("id", user_id).execute()
 
 
 
