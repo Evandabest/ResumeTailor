4a6641aa01e9548d2a0d1fc87be753d2f3bc8159
sdemetriusjenkins <sdemetriusjenkins@gmail.com>
2025-03-17 20:47:40 -0400

    Moved LoginPage.test.tsx

--- frontend/__tests__/unit_tests_results/LoginPage.test.tsx
+++ frontend/__tests__/unit_tests_results/LoginPage.test.tsx
@@ -1,5 +1,5 @@
 import { render, screen, fireEvent, waitFor } from '@testing-library/react'
-import LoginPage from '../app/login/page'
+import LoginPage from '../../app/login/page'
 import { useRouter } from 'next/navigation'
 
 // Mock next/navigation
