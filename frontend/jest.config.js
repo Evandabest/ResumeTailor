97657f2a033df19faf7c30a729a8abc8cb56e87f
sdemetriusjenkins <sdemetriusjenkins@gmail.com>
2025-03-17 22:09:54 -0400

    Added integration tests for login form components: - Test expected interaction between Tab and Form Input components - Test error handling between form components

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -3,15 +3,9 @@ module.exports = {
   setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/$1',
-    '\\.(css|less|scss|sass)$': 'identity-obj-proxy'
   },
   testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
   transform: {
     '^.+\\.(js|jsx|ts|tsx)$': ['babel-jest', { presets: ['next/babel'] }]
-  },
-  collectCoverageFrom: [
-    'app/**/*.{js,jsx,ts,tsx}',
-    '!app/**/_*.{js,jsx,ts,tsx}',
-    '!app/**/*.d.ts'
-  ]
+  }
 }
