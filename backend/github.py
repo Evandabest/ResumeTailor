aa981a181631f53e5350e4218403c3c533bbab23
DUOLabs333 <dvdugo333@gmail.com>
2025-03-28 15:37:06 -0400

    Get CRUD operations on GitHub integration working

--- backend/github.py
+++ backend/github.py
@@ -1,7 +1,28 @@
 from .utils import *
 
-@endpoint("/github/link", ["code"])
+@endpoint("/github/link", ["code"]) #Used for both linking, and updating
 def link():
     #Code must be a GitHub token with at least read access to repositories (you can decide which repos to leave out by using the Fine-grained PAT page) 
+
+    #Assumes code is valid
     User.table("user_to_token").upsert({"id": get_id_from_token(token), "token": code}).execute()
+
+#For internal use
+def _token(token):
+    lst=User.table("user_to_token").select("token").eq("id", get_id_from_token(token)).execute().data
+    if len(lst)==0:
+        return ""
+    else:
+        return lst[0]["token"]
+
+#Is used solely for displaying the token in the settings page
+@endpoint("/github/token", [], ["code"])
+def token_():
+    code=_token(token)
+
+@endpoint("/github/unlink", []) #We *could* make it so that to unlink a token, you just send a blank code to link
+def unlink():
+    User.table("user_to_token").delete().eq("id", get_id_from_token(token)).execute()
+    
+
 
--- backend/tests/github.py
+++ backend/tests/github.py
@@ -11,9 +11,41 @@ def setup(client):
 
     credentials={"token": token}
 
-def test_link_valid(client):
-    #Assumes token is valid
-    print(client.post("/github/link", json=credentials | {"code": config["TEST_USER_TOKEN"]}).json)
+def teardown(client):
+    ignore_asserts(test_unlink)(client)
+
+def test_link(client):
+    is_success(client.post("/github/link", json=credentials | {"code": config["TEST_USER_TOKEN"]}))
+
+    pass
+
+def test_token(client):
+    assert is_success(client.post("/github/token", json=credentials))["code"]==config["TEST_USER_TOKEN"]
+
+def test_link_update(client):
+    """
+    If a user tries to update their token, it should succeed
+    """
+
+    is_success(client.post("/github/link", json=credentials | {"code": "Testing123"}))
+
+    assert is_success(client.post("/github/token", json=credentials))["code"]=="Testing123"
+    
+    is_success(client.post("/github/link", json=credentials | {"code": config["TEST_USER_TOKEN"]}))
+
+def test_unlink(client):
+    is_success(client.post("/github/unlink", json=credentials))
+
+    pass
+
+def test_token_blank(client):
+    """
+    If a user tries to view their token after unlinking it, they should get an empty string
+    """
+
+    assert is_success(client.post("/github/token", json=credentials))["code"]==""
+
+
 
     
     
