dca2174a6324fd292c1565e549ca4e67116aa6af
DUOLabs333 <dvdugo333@gmail.com>
2025-03-30 08:06:09 -0400

    Create tests for the /list and /import Github endpoints

--- backend/tests/github.py
+++ backend/tests/github.py
@@ -45,6 +45,32 @@ def test_token_blank(client):
 
     assert is_success(client.post("/github/token", json=credentials))["code"]==""
 
+repos=[]
+def test_list_projects(client):
+    global repos
+    """
+    If a user tries to list all of their GitHub projects after linking an account, it should succeed
+    """
+
+    repos=is_success(client.post("/github/projects/list", json=credentials))["repos"]
+
+    assert len(repos)>0 #Assumes that the test user used has at least one repo in their account
+
+def test_import_projects_valid(client):
+    """
+    If a user tries to import their GitHub projects, it should succeed
+    """
+
+    assert is_success(client.post("/github/projects/import", json=credentials | {"repos": [repo["name"] for repo in repos]}))
+
+def test_import_projects_invalid(client):
+    """
+    If a user tries to import a repo that they do not own, or does not exist, it should fail
+    """
+    for repos in [["kubernetes/kubernetes"], ["fhgidfgfudfb"]]:
+        assert is_error(client.post("/github/projects/import", json=credentials| {"repos": repos}))
+
+
 
 
     
