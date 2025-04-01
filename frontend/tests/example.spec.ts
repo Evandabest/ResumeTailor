39ab5b9fd2e0ac256ecb185fc4c08b9aa7774626
Evandabest <evanhaque1@gmail.com>
2025-04-01 17:44:50 -0400

    fix: remove unecessary files

--- frontend/tests/example.spec.ts
+++
@@ -1,7 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-test('hello world test', async ({ page }: { page: import('@playwright/test').Page }) => {
-    await page.goto('https://example.com');
-    const title = await page.title();
-    expect(title).toBe('Example Domain');
-});
