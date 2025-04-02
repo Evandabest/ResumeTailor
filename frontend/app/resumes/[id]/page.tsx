45f034c82174124533fd76abf8ec6148a8170f23
Evandabest <145516605+Evandabest@users.noreply.github.com>
2025-04-02 07:54:58 -0400

    Github unit/integration/end to end tests (#10)
    * chore: add shadcn to project
    
    * feat: make and implement navbar
    
    * feat: landing page
    
    * feat: login page
    
    * feat: dashboard page
    
    * feat: add latex rendering pages (not working)
    
    * added info for tests
    
    * Update README_TESTS.md
    
    removed CI section
    
    * feat: local/in house latex compiler
    
    * chore: add more shacn components
    
    * fix: adjust routing to latex compiler
    
    * chore: add shadcn components
    
    * feat: add resume pages and implement compiler
    
    * feat: codecov and jest set up
    
    * test: add test for dashboard (unit)
    
    * chore: add user-event jest library
    
    * test: integration test between tabs on dashboard
    
    * chore: add playwright and remove babel
    
    * feat: set up playwright
    
    * test: implement end to end user test
    
    * test: add test checks to github actions
    
    * fix: potential fix for test.yml
    
    * fix: disable eslint checking
    
    * fix: add missing page
    
    * chore: update command component from shadcn
    
    * fix: update nextjs version due to security issues with vercel
    
    * fix: remove unecessary files
    
    * fix: update test configuration and behaviors
    
    * fix: adjust for tests in ci/cd
    
    * fix: adjust test.yml
    
    * fix: test will run on any branch
    
    * fix: reattempt to merge with new package json
    
    ---------
    
    Co-authored-by: Avirup Ray <aray002@citymail.cuny.edu>
    Co-authored-by: Avirup Ray <136357304+aray1337@users.noreply.github.com>

---
+++ frontend/app/resumes/[id]/page.tsx
@@ -0,0 +1,12 @@
+
+
+const ResumePreview = () => {
+    return (
+        <div>
+        <h1>Resume Preview</h1>
+        <p>This is the resume preview page.</p>
+        </div>
+    );
+}
+
+export default ResumePreview;
