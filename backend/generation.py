a02473fca3073282df4362526973d683a13d4ca2
DUOLabs333 <dvdugo333@gmail.com>
2025-04-23 08:13:21 -0400

    * Rename "id" column to "uid", and "instance_id" to "id" 	The original naming was not very clear, and was only done to align with the auth.users table. However, upon further reflection, the meaning of "id" in auth.users and other tables are different (user id and id of row, respectively), and therefore the schema of the latter table should not be contorted to fit the schema of the former.
    * Finish preliminary version of the synthesis/generation endpoints

---
+++ backend/generation.py
@@ -0,0 +1,66 @@
+from .utils import *
+
+@endpoint("/generate/rag", ["job_listing"], ["repos"])
+def rag():
+    #By default, all of the returned repos should be checked on the frontend
+
+    ids=User.rpc("match_projects", {"uid": get_id_from_token(token), "query_embedding": get_embeddings(token, [job_listing])[0], "minimum_score": 0.5, "count": 10}).execute().data #Can tweak the minimum and/or count
+
+    repos=User.table.select("id, name, url").in_("id", ids).execute().data
+
+@endpoint("/generate/points", ["ids", "job_listing"], ["output"])
+def points():
+    #The user should be asked if they want to recreate the points, as well as edit the points manually if needed
+
+    projects=User.table("user_to_projects").select("text").and_(f"id.in.{tuple(ids)}, uid.eq.{get_id_from_token(token)}").execute().data
+
+    output=llm(token,
+    f"""
+    You are helping to format a user's GitHub projects so that it can be inserted into their resume.
+
+    Here is the job listing:
+    
+    {job_listing}
+
+    Here are some potentially relevant projects that the user has selected for inclusion in their resume:
+
+    {"\n\n".join([project["text"] for project in projects])}
+
+    Summarize the list of projects into a list of projects that can be inserted into the resume. You are free to remove any project from the list if they are not in fact relevant.
+
+    It is imperative that you ONLY return a bullet point list, nothing else.
+    """
+    )
+
+
+@endpoint("/generate/latex", ["input", "resume_id"], ["output"])
+def latex():
+    #The user should be asked if they want to recreate/edit the returned latex code
+    #llm should strip first
+
+
+    output=llm(token,
+    f"""
+    You are editing the resume of a user to include some of their personal GitHub projects.
+
+    Here is the LaTeX resume they want to edit:
+
+    {User.table("user_to_resume").select("content").and_(f"id.eq.{resume_id}, uid.eq.{get_id_from_token(token)}").execute().data[0]["content"]}
+
+    Here is the bullet point list they want to include:
+
+    {input}
+
+    Integrate the bullet point list into the resume in a way that maintains the cohesion of the resume's theme, style, and structure --- do not just paste in the list without any consideration.
+
+    Return JUST the modified resume, nothing more.
+    """)
+
+@endpoint("/endpoint/pdf", ["filename", "resume_id", "content"], [File("output")] )
+def pdf():
+    if filename is None:
+        filename=User.table("user_to_resume").select("content").and_(f"id.eq.{resume_id}, uid.in.user", resume_id).execute().data[0]["content"]
+
+    
+    
+
--- backend/resume.py
+++ backend/resume.py
@@ -7,9 +7,9 @@ table=User.table("user_to_resume")
 
 def validate_id(id): #Checks whether the resume referenced by the id has permission to be accessed by the user in the token
     token=inspect.currentframe().f_back.f_locals["token"]
-    data=table.select("id").eq("instance_id", id).execute().data
-    if ((len(data)==0) or (data[0]["id"]!=get_id_from_token(token))):
-        raise ValueError("Current user does not have permission to access the resume at the id")
+    data=table.select("uid").eq("id", id).execute().data
+    if ((len(data)==0) or (data[0]["uid"]!=get_id_from_token(token))):
+        raise ValueError("Current user does not have permission to access the resume at the given id")
 
 @endpoint("/resume/upload", [File("file"), "update"])
 def upload():
@@ -18,23 +18,23 @@ def upload():
     if not filename.endswith(".tex"):
         raise ValueError("Only .tex files can be uploaded")
 
-    data={"id": get_id_from_token(token), "content": file.read().decode(), "filename": filename}
+    data={"uid": get_id_from_token(token), "content": file.read().decode(), "filename": filename}
 
     if update is None:
         action=table.insert(data)
     else:
         validate_id(update)
-        action=table.update(data).eq("instance_id", update)
+        action=table.update(data).eq("id", update)
     action.execute()
 
 @endpoint("/resume/delete", ["id"])
 def delete():
     validate_id(id)
-    table.delete().eq("instance_id", id).execute()
+    table.delete().eq("id", id).execute()
 
 @endpoint("/resume/list", [], ["data"])
 def list():
-    data=table.select("id:instance_id, filename").execute().data
+    data=table.select("id, filename").execute().data
 
 @endpoint("/resume/rename", ["id", "name"])
 def rename():
@@ -42,6 +42,6 @@ def rename():
         raise ValueError("New name must end in '.tex'")
 
     validate_id(id)
-    table.update({"filename": os.path.basename(name)}).eq("instance_id", id).execute()
+    table.update({"filename": os.path.basename(name)}).eq("id", id).execute()
     
     
